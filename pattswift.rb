11ccde34e5db7f4c4bb88ff785468503421763be
lirummirul <lada.yakovleva.2018@mail.ru>
2024-05-23 20:14:25 +0300

    add formula

---
+++ pattswift.rb
@@ -0,0 +1,10 @@
+class Pattswift < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/lirummirul/PattSwift/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "91034536355e96da2d3a894b30305b7980206fd91952adce38c7f7b695aa4eae"
+
+  def install
+    bin.install "pattsw.sh"
+  end
+end
