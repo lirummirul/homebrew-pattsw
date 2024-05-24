0a16f2aaff77861ee29abb681e064d714229370c
lirummirul <lada.yakovleva.2018@mail.ru>
2024-05-24 21:09:02 +0300

    add v1.0.5

--- pattswift.rb
+++ pattswift.rb
@@ -1,8 +1,8 @@
 class Pattswift < Formula
   desc "Generate files from templates"
   homepage "https://github.com/lirummirul/PattSwift"
-  url "https://github.com/lirummirul/PattSwift/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "658a6d87b7c2b89f257bf5c93eeba4962d3cae4940beee4c94829d524975e99a"
+  url "https://github.com/lirummirul/PattSwift/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "49f91603f772c16d9974de1a6df815c7ed5eddd9a2dc7972f680410da63d6a56"
   
   def install
     bin.install "pattsw"
