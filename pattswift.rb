678fcd3c95df9ce398ebce04587a4a500a2ea51d
lirummirul <lada.yakovleva.2018@mail.ru>
2024-05-24 18:06:38 +0300

    fix version url

--- pattswift.rb
+++ pattswift.rb
@@ -1,7 +1,7 @@
 class Pattswift < Formula
   desc ""
   homepage ""
-  url "https://github.com/lirummirul/PattSwift/archive/refs/tags/v1.0.2.tar.gz"
+  url "https://github.com/lirummirul/PattSwift/archive/refs/tags/v1.0.3.tar.gz"
   sha256 "91034536355e96da2d3a894b30305b7980206fd91952adce38c7f7b695aa4eae"
 
   def install
