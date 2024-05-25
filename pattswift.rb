d7f0d4758063b48791d30ed73f4bf19b17de0f5d
lirummirul <lada.yakovleva.2018@mail.ru>
2024-05-25 15:54:40 +0300

    fix style

--- pattswift.rb
+++ pattswift.rb
@@ -3,7 +3,7 @@ class Pattswift < Formula
   homepage "https://github.com/lirummirul/PattSwift"
   url "https://github.com/lirummirul/PattSwift/archive/refs/tags/v1.0.5.tar.gz"
   sha256 "49f91603f772c16d9974de1a6df815c7ed5eddd9a2dc7972f680410da63d6a56"
-  
+
   def install
     bin.install "pattsw"
   end
